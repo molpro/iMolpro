bd9f4471dc592bb5ab8d3071a6d06d6ea1150060
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-07-25 17:21:40 +0100

    initial

---
+++ QtMolpro.py
@@ -0,0 +1,208 @@
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPlainTextEdit, QVBoxLayout, QHBoxLayout, QWidget, \
+    QPushButton
+from PyQt5.QtGui import QFont, QFontDatabase
+from pymolpro import Project
+import sys
+import os
+import pathlib
+
+
+class EditFile(QPlainTextEdit):
+    def __init__(self, filename: str, latency=1000):
+        super().__init__()
+        self.filename = str(filename)
+        if os.path.isfile(self.filename):
+            with open(self.filename, 'r') as f:
+                self.savedText = f.read()
+        else:
+            self.savedText = ''
+        self.setPlainText(self.savedText)
+        f = QFont(QFontDatabase.systemFont(QFontDatabase.FixedFont))
+        f.setPointSize(12)
+        self.setFont(f)
+
+        import atexit
+        atexit.register(self.flush)
+        self.flushTimer = QTimer()
+        self.flushTimer.timeout.connect(self.flush)
+        self.flushTimer.start(latency)
+
+    def flush(self):
+        current = self.toPlainText()
+        if current != self.savedText:
+            with open(self.filename, 'w') as f:
+                f.write(current)
+            self.savedText = current
+        return
+
+
+class ViewFile(QPlainTextEdit):
+    def __init__(self, filename: str, latency=1000):
+        super().__init__()
+        self.setReadOnly(True)
+        self.latency = latency
+        self.reset(filename)
+        f = QFont(QFontDatabase.systemFont(QFontDatabase.FixedFont))
+        f.setPointSize(10)
+        self.setFont(f)
+
+    def refresh(self):
+        scrollbar = self.verticalScrollBar()
+        scrollbarAtBottom = scrollbar.value() >= (scrollbar.maximum() - 4)
+        scrollbarPrevValue = scrollbar.value()
+        if os.path.isfile(self.filename):  # should not do this if the file hasn't changed
+            with  open(self.filename, 'r') as f:  # TODO make more efficient, and read only needed updates
+                self.setPlainText(f.read())  # should just read the increment
+            if scrollbarAtBottom:
+                self.verticalScrollBar().setValue(scrollbar.maximum())
+            else:
+                self.verticalScrollBar().setValue(scrollbarPrevValue)
+        return
+
+    def reset(self, filename):
+        self.filename = str(filename)
+        self.savedText = ''
+        self.refreshTimer = QTimer()
+        self.refreshTimer.timeout.connect(self.refresh)
+        self.refreshTimer.start(self.latency)
+        return
+
+
+class StatusBar(QLabel):
+    def __init__(self, project, latency=1000):
+        super().__init__()
+        self.project = project
+        self.refreshTimer = QTimer()
+        self.refreshTimer.timeout.connect(self.refresh)
+        self.refreshTimer.start(latency)
+
+    def refresh(self):
+        self.setText('Status: ' + ('run ' + pathlib.Path(
+            self.project.filename()).stem + ' ' if self.project.filename() != self.project.filename(
+            run=-1) else '') + self.project.status)
+
+
+def launchExternalViewer(file):
+    import subprocess
+    subprocess.Popen(['jmol', file])
+
+
+class ProjectWindow(QMainWindow):
+    def __init__(self, filename=None, latency=1000):
+        super().__init__()
+
+        assert filename is not None  # TODO eventually pop dialog for this
+
+        self.project = Project(filename)  # TODO some error checking needed
+        self.inputPane = EditFile(self.project.filename('inp', run=-1), latency)
+        self.setWindowTitle(filename)
+        self.runButton = QPushButton('Run')
+        self.runButton.clicked.connect(self.run)
+        self.visoutButton = QPushButton('Visualise output')
+        self.visoutButton.clicked.connect(lambda: self.visout('xml'))
+        self.visinpButton = QPushButton('Visualise input')
+        self.visinpButton.clicked.connect(self.visinp)
+
+        self.statusBar = StatusBar(self.project)
+
+        leftLayout = QVBoxLayout()
+        leftLayout.addWidget(self.inputPane)
+        self.inputPane.setMinimumHeight(300)
+        self.inputPane.setMinimumWidth(400)
+        self.statusBar.setMaximumWidth(400)
+        buttonLayout = QHBoxLayout()
+        buttonLayout.addWidget(self.visinpButton)
+        buttonLayout.addWidget(self.runButton)
+        buttonLayout.addWidget(self.visoutButton)
+        putButtons = []
+
+        class visoutButton(QPushButton):
+            def __init__(self, name):
+                super().__init__(name)
+
+            def setAction(self, f):
+                self.action = f
+                self.clicked.connect(lambda: self.action('',self.text()))
+
+        for t, f in self.putfiles():
+            putButtons.append(visoutButton(f))
+            putButtons[-1].setAction(self.visout)
+            buttonLayout.addWidget(putButtons[-1])
+        leftLayout.addLayout(buttonLayout)
+        leftLayout.addWidget(self.statusBar)
+
+        self.outputPane = ViewFile(self.project.filename('out'), latency=100)
+        self.outputPane.setMinimumWidth(800)
+        self.refreshOutputFileTimer = QTimer()
+        self.refreshOutputFileTimer.timeout.connect(self.refreshOutputFile)
+        self.refreshOutputFileTimer.start(2000)  # find a better way
+
+        layout = QHBoxLayout()
+        layout.addLayout(leftLayout)
+        layout.addWidget(self.outputPane)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+    def putfiles(self):
+        result = []
+        lines = self.inputPane.toPlainText().replace(';', '\n').split('\n')
+        for line in lines:
+            fields = line.replace(' ', ',').split(',')
+            if len(fields) > 2 and fields[0].lower() == 'put':
+                result.append((fields[1], fields[2]))
+        return result
+
+    def refreshOutputFile(self):
+        self.outputPane.reset(self.project.filename('out'))
+
+    def run(self):
+        self.project.run()
+
+    def visout(self, typ='xml', name=None):
+        # filename = self.project.filename('molden')
+        # with open(filename, 'w') as f:
+        #     f.write(to_molden(self.project))
+        # launchExternalViewer(filename)
+        if name:
+            launchExternalViewer(self.project.filename(typ, name))
+        else:
+            launchExternalViewer(self.project.filename(typ))
+
+    def visinp(self):
+        import tempfile
+        geometry_directory = pathlib.Path(self.project.filename(run=-1)) / 'initial'
+        geometry_directory.mkdir(exist_ok=True)
+        xyzFile = str(geometry_directory / pathlib.Path(self.project.filename(run=-1)).stem) + '.xyz'
+        if not os.path.isfile(xyzFile) or os.path.getmtime(xyzFile) < os.path.getmtime(
+                self.project.filename('inp', run=-1)):
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                self.project.copy(pathlib.Path(self.project.filename(run=-1)).name, location=tmpdirname)
+                project_path = pathlib.Path(tmpdirname) / pathlib.Path(self.project.filename(run=-1)).name
+                project = Project(str(project_path))
+                project.clean(0)
+                open(project.filename('inp', run=-1), 'a').write('---')
+                with open(pathlib.Path(project.filename(run=-1)) / 'molpro.rc', 'a') as f:
+                    f.write(' --geometry')
+                project.run(wait=True, force=True, backend='local')
+                geometry = project.geometry()
+                with open(xyzFile, 'w') as f:
+                    f.write(str(len(geometry)) + '\n\n')
+                    for atom in geometry:
+                        f.write(atom['elementType'])
+                        for c in atom['xyz']: f.write(' ' + str(c * .529177210903))
+                        f.write('\n')
+        launchExternalViewer(xyzFile)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    windows = []
+    for arg in sys.argv[1:]:
+        windows.append(ProjectWindow(arg))
+        windows[-1].show()
+
+    app.exec()
---
+++ obsolete.py
@@ -0,0 +1,85 @@
+def atomic_number(el):
+    elements = ['X', 'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
+                'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar',
+                'K', 'Ca',
+                'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
+                'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
+                'Rb', 'Sr',
+                'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd',
+                'In', 'Sn', 'Sb', 'Te', 'I', 'Xe',
+                'Cs', 'Ba',
+                'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb',
+                'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
+                'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn',
+                'Fr', 'Ra',
+                'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No',
+                'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn',
+                'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+    return elements.index(el)
+
+
+
+def to_molden(project: Project, vibrations_instance=-1, orbitals_instance=-1):
+    nodes = project.xpath('//vibrations')
+    if len(nodes) < 1 or vibrations_instance >= len(nodes) or vibrations_instance < -len(nodes):
+        vibrations = False
+        # anchor to orbitals instead
+        nodes = project.xpath('//orbitals')
+        if len(nodes) < 1 or orbitals_instance >= len(nodes) or orbitals_instance < -len(nodes): return None
+        orbitals_node = nodes[orbitals_instance]
+    else:
+        vibrations = True
+        anchor = nodes[vibrations_instance]
+        nodes = project.xpath('//orbitals', anchor)
+        orbitals_node = nodes[0] if len(
+            nodes) > 0 else None  # assumes orbital calculation will be done right after vibrations
+    geometry = project.xpath('//preceding::cml:atomArray', anchor)[-1]
+    result = '[Molden Format]\n[Atoms] Angs\n'
+    i = 0
+    for atom in geometry:
+        i += 1
+        result += atom.get('elementType') + ' ' + str(i) + ' ' + str(
+            atomic_number(atom.get('elementType'))) + ' ' + atom.get('x3') + ' ' + atom.get('y3') + ' ' + atom.get(
+            'z3') + '\n'
+    if vibrations:
+        modes = list(reversed(project.xpath('normalCoordinate', anchor)))
+        print('found', len(modes), 'normal coordinates')
+        result += ' [FREQ]\n'
+        for mode in modes:
+            result += '    ' + str(mode.get('wavenumber')) + '\n'
+        result += ' [FR-COORD]\n'
+        for atom in geometry:
+            result += atom.get('elementType') + ' ' + str(1.8897261246*float(atom.get('x3'))) + ' ' + str(1.8897261246*float(atom.get('y3'))) + ' ' + str(1.8897261246*float(atom.get( 'z3'))) + '\n'
+        result += ' [FR-NORM-COORD]\n'
+        i = 0
+        for mode in modes:
+            i += 1
+            result += ' Vibration ' + str(i) + mode.text
+        result += '[INT]\n'
+        for mode in modes:
+            result += '  ' + mode.get('IRintensity') + '\n'
+    print(result)
+    return result
+
+# def vibrations_cjson(project: Project, instance=-1):
+#     result = {'chemicalJson': 1}
+#     nodes = project.xpath('//vibrations')
+#     if len(nodes) < 1 or instance >= len(nodes) or instance < -len(nodes): return None
+#     print(len(nodes))
+#     print(nodes[instance])
+#     modes = project.xpath('normalCoordinate', nodes[instance])
+#     print('found', len(modes), 'normal coordinates')
+#     geometry = project.xpath('//preceding::cml:atomArray', nodes[instance])[-1]
+#     print('found', len(geometry), 'geometry')
+#     result['atoms'] = {'coords': {'3d': []}, 'elements': {'number': []}}
+#     for atom in geometry:
+#         print(atom.get('x3'))
+#         result['atoms']['coords']['3d'].append(atom.get('x3'))
+#         result['atoms']['coords']['3d'].append(atom.get('y3'))
+#         result['atoms']['coords']['3d'].append(atom.get('z3'))
+#         # result['atoms']['elements'].
+#     print(result)
+#     import json
+#     return json.dumps(result)
+
+
