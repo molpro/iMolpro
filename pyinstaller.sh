98056b83b136e1e69564115bf2f4ab9bdcc67075
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-09-13 18:40:08 +0100

    Rename the app QtMolpro -> Molpro (#20)

--- pyinstaller.sh
+++ pyinstaller.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $(uname) = Darwin ]; then
-  brew install qt@5
+  brew install qt@5 create-dmg
 else
   echo 'make sure Qt5 C++ libraries are installed'
 fi
@@ -11,23 +11,24 @@ pip install pyinstaller PyQtWebEngine
 
 
 if [ $(uname) = Darwin ]; then
-  pyinstaller_opt="--windowed --osx-bundle-identifier=net.molpro.Qtmolpro --icon=molpro.icns"
+  pyinstaller_opt="--windowed --osx-bundle-identifier=net.molpro.Molpro --icon=molpro.icns"
 fi
-builddir=${TMPDIR:-/tmp}/QtMolpro
+builddir=${TMPDIR:-/tmp}/Molpro
 rm -rf $builddir
 
 PATH=/usr/bin:$PATH pyinstaller \
   --add-data JSmol.min.js:. \
   --add-data j2s:./j2s \
   --add-data Molpro_Logo_Molpro_Quantum_Chemistry_Software.png:. \
+  --add-data README.md:. \
+  --add-data doc:./doc \
   --distpath $builddir/dist $pyinstaller_opt \
-  QtMolpro.py || exit 1
+  Molpro.py || exit 1
 
 if [ $(uname) = Darwin ]; then
-  rm -rf $builddir/dist/QtMolpro
-  hdiutil create $builddir/QtMolpro.dmg -ov -volname 'QtMolpro' -fs HFS+ -srcfolder "$builddir/dist"
-  rm -f QtMolpro.dmg
-  hdiutil convert $builddir/QtMolpro.dmg -format UDZO -o QtMolpro.dmg
+  rm -rf $builddir/dist/Molpro
+  rm -f Molpro.dmg
+  create-dmg --app-drop-link 25 35 --volname Molpro  --volicon 'Molpro_Logo_Molpro_Quantum_Chemistry_Software.png' Molpro.dmg "$builddir/dist"
 else
   rm -rf dist build
   mv $builddir/dist .
