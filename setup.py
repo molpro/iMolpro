51d6158e9555b95d0adead0e8a32da89f04d62e9
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-09-18 23:10:23 +0100

    versioneer

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import versioneer
+
+if __name__ == "__main__":
+    setup(
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+    )
