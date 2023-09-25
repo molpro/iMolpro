a082ab15e4de82ae1ef8eb57f6a0771c171a2de2
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-09-25 07:38:28 +0100

    ChemSpider database search

--- database.py
+++ database.py
@@ -2,13 +2,15 @@ import os
 import pathlib
 import sys
 import pubchempy
+from chemspipy import ChemSpider
+from openbabel import pybel
 import tempfile
-from PyQt5.QtWidgets import QVBoxLayout, QDialog, QDialogButtonBox, QLabel, QComboBox, QLineEdit
+from PyQt5.QtWidgets import QVBoxLayout, QDialog, QDialogButtonBox, QLabel, QComboBox, QLineEdit, QCheckBox, QHBoxLayout
 
 
 class DatabaseSearchDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self):
+        super().__init__()
         self.setWindowTitle('Search online structure databases')
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
@@ -19,6 +21,19 @@ class DatabaseSearchDialog(QDialog):
         self.value.setPlaceholderText('Enter search text here')
         self.layout.addWidget(self.value)
 
+        self.pubChemCheckBox = QCheckBox(self)
+        self.pubChemCheckBox.setText('PubChem')
+        self.pubChemCheckBox.setChecked(True)
+        self.chemSpiderCheckBox = QCheckBox(self)
+        self.chemSpiderCheckBox.setText('ChemSpider')
+        self.chemSpiderCheckBox.setChecked('CHEMSPIDER_API_KEY' in os.environ)
+        checkBoxLayout = QHBoxLayout()
+        checkBoxLayout.addWidget(QLabel('Databases: '))
+        checkBoxLayout.addWidget(self.pubChemCheckBox)
+        checkBoxLayout.addWidget(self.chemSpiderCheckBox)
+        checkBoxLayout.addStretch()
+        self.layout.addLayout(checkBoxLayout)
+
         self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
@@ -26,8 +41,8 @@ class DatabaseSearchDialog(QDialog):
 
 
 class DatabaseFetchDialog(QDialog):
-    def __init__(self, query, parent=None, usePubChem=True, useChemSpider=True):
-        super().__init__(parent)
+    def __init__(self, query, usePubChem=True, useChemSpider=True):
+        super().__init__()
         self.setWindowTitle('Select from database search results')
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
@@ -41,7 +56,7 @@ class DatabaseFetchDialog(QDialog):
                 return str(i) + ' matches'
 
         if usePubChem:
-            self.database='PubChem'
+            self.database = 'PubChem'
             for field in ['name', 'cid', 'inchi', 'inchikey', 'sdf', 'smiles', 'formula', ]:
                 if field == 'cid' and not all(chr.isdigit() for chr in query.strip()): continue
                 if field == 'inchi' and query.strip()[:3] != '1S/': continue
@@ -54,11 +69,29 @@ class DatabaseFetchDialog(QDialog):
                     self.layout.addWidget(self.buttonBox)
                     return
                 if self.compounds: break
-            self.layout.addWidget(QLabel('PubChem found ' + matches(len(self.compounds)) + ' to ' + field + '=' + query))
+            self.layout.addWidget(
+                QLabel('PubChem found ' + matches(len(self.compounds)) + ' to ' + field + '=' + query))
             if self.compounds:
                 self.chooser = QComboBox()
                 self.chooser.addItems(
-                    [str(result.cid) + ' (' + ', '.join(result.synonyms)[:50] + '...)' for result in self.compounds])
+                    [str(compound.cid) + ' (' + ', '.join(compound.synonyms)[:50] + '...)' for compound in
+                     self.compounds])
+                self.layout.addWidget(self.chooser)
+                self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+                self.buttonBox.accepted.connect(self.accept)
+                self.buttonBox.rejected.connect(self.reject)
+                self.layout.addWidget(self.buttonBox)
+                return
+
+        if useChemSpider and (cs := ChemSpider(os.environ['CHEMSPIDER_API_KEY'])):
+            self.database = 'ChemSpider'
+            self.compounds = cs.search(query)
+            self.layout.addWidget(
+                QLabel('ChemSpider found ' + matches(len(self.compounds)) + ' for ' + query))
+            if self.compounds:
+                self.chooser = QComboBox()
+                self.chooser.addItems(
+                    [str(compound.csid) + ' (' + compound.common_name + ')' for compound in self.compounds])
                 self.layout.addWidget(self.chooser)
                 self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
                 self.buttonBox.accepted.connect(self.accept)
@@ -77,17 +110,26 @@ class DatabaseFetchDialog(QDialog):
             return f'{x:.8f}'
 
         compound = self.compounds[index_]
-        record = compound.record
-        conformer = record['coords'][0]['conformers'][0]
-        xyz = str(len(compound.elements)) + '\n' + 'PubChem cid=' + str(compound.cid) + '\n'
-        for key, element in enumerate(compound.elements):
-            xyz += element + ' ' + s(conformer['x'][key]) + ' ' + s(conformer['y'][key]) + ' ' + \
-                   s(conformer['z'][key]) + '\n'
-        return xyz
+
+        if self.database == 'PubChem':
+            record = compound.record
+            conformer = record['coords'][0]['conformers'][0]
+            xyz = str(len(compound.elements)) + '\n' + 'PubChem cid=' + str(compound.cid) + '\n'
+            for key, element in enumerate(compound.elements):
+                xyz += element + ' ' + s(conformer['x'][key]) + ' ' + s(conformer['y'][key]) + ' ' + \
+                       s(conformer['z'][key]) + '\n'
+            return xyz
+
+        if self.database == 'ChemSpider':
+            molecule = pybel.readstring('SDF', compound.mol_3d)
+            return molecule.write('XYZ')
 
     def cid(self, index=None):
         index_ = index if index else self.chooser.currentIndex()
-        return self.compounds[index_].cid
+        if self.database == 'PubChem':
+            return self.compounds[index_].cid
+        elif self.database == 'ChemSpider':
+            return self.compounds[index_].csid
 
 
 def database_choose_structure():
@@ -98,9 +140,9 @@ def database_choose_structure():
     dlg = DatabaseSearchDialog()
     dlg.exec()
     if dlg.result():
-        dlg2 = DatabaseFetchDialog(dlg.value.text())
+        dlg2 = DatabaseFetchDialog(dlg.value.text(),dlg.pubChemCheckBox.isChecked(),dlg.chemSpiderCheckBox.isChecked())
         dlg2.exec()
         if dlg2.result():
-            filename = pathlib.Path(tempfile.mkdtemp()) / (dlg2.database() + '-' + str(dlg2.cid()) + '.xyz')
+            filename = pathlib.Path(tempfile.mkdtemp()) / (dlg2.database + '-' + str(dlg2.cid()) + '.xyz')
             open(filename, 'w').write(dlg2.xyz())
             return filename
