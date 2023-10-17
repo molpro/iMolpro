13c993e2ad239f68f9dcf45059ea90945dbe0396
doll <doll@theochem.uni-stuttgart.de>
2023-10-17 16:53:21 +0200

    fix crash if no method entered, similar to case of basis set

--- ProjectWindow.py
+++ ProjectWindow.py
@@ -307,7 +307,8 @@ class ProjectWindow(QMainWindow):
     def refresh_guided_pane(self):
         if self.trace: print('refresh_guided_pane')
         self.guided_basis_input.setText(self.input_specification['basis'])
-        self.guided_combo_method.setCurrentText(self.input_specification['method'])
+        if 'method' in self.input_specification:
+            self.guided_combo_method.setCurrentText(self.input_specification['method'])
         self.guided_display.setText(
             re.sub('}$', '\n}', re.sub('^{', '{\n  ', str(self.input_specification))).replace(', ',
                                                                                               ',\n  '))  # TODO this will eventually be removed
