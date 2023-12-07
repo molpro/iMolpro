9935dc33cc9868a422603d192319bc183efc53b8
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-12-07 09:42:13 +0000

    First implementation of backend editor

--- backend.py
+++ backend.py
@@ -74,39 +74,93 @@ class BackendConfigurationEditor(QDialog):
         # win.setMinimumSize(600, 400)
         self.file = file
 
-        backends_ = [backend.get('name') for backend in (etree.parse(file).xpath('//backend'))]
-        self.layout = QFormLayout()
+        self.layout = QVBoxLayout()
+        form_layout = QFormLayout()
+        self.layout.addLayout(form_layout)
+        self.setWindowTitle('Configuration of backends')
         self.edit_combo = QComboBox()
         self.edit_combo.addItem('')
-        self.edit_combo.addItems(backends_)
-        self.edit_combo.currentTextChanged.connect(lambda text, file=self.file: edit(text, file))
-        self.layout.addRow('Edit backend', self.edit_combo)
+        self.edit_combo.addItems(self.backends)
+        self.edit_combo.currentTextChanged.connect(self.edit)
+        form_layout.addRow('Edit or delete:', self.edit_combo)
         self.new_combo = QComboBox()
         self.new_combo.addItems(['', 'local', 'remote linux', 'Slurm', 'Other'])
-        self.new_combo.currentTextChanged.connect(lambda text, file=self.file: new(text, file))
-        self.layout.addRow('New backend', self.new_combo)
+        self.new_combo.currentTextChanged.connect(self.new)
+        form_layout.addRow('New:', self.new_combo)
+        buttons = QDialogButtonBox(QDialogButtonBox.Ok)
+        buttons.accepted.connect(self.close)
+        self.layout.addWidget(buttons)
         self.setLayout(self.layout)
 
+    @property
+    def backends(self):
+        backends_ = [backend.get('name') for backend in (etree.parse(self.file).xpath('//backend'))]
+        return backends_
 
-def edit(text, file):
-    print('backend.edit', text, file)
+    def edit(self, text):
+        if not text: return
+        dlg = BackendEditor(text, self)
+        result = dlg.exec()
+        self.edit_combo.setCurrentText('')
+
+    def new(self, text):
+        if not text: return
+        root = etree.parse(self.file)
+        n = etree.SubElement(root.getroot(), 'backend')
+        sequence = 1
+        while text.replace(' ', '_') + '_' + str(sequence) in self.backends:
+            sequence += 1
+        name = text.replace(' ', '_') + '_' + str(sequence)
+        n.set('name', name)
+        n.set('run_command', 'molpro')
+        if text == 'remote linux':
+            n.set('host', 'someone@some.computer.somewhere')
+        if text == 'Slurm':
+            n.set('run_command', 'your_job_submission_script')
+            n.set('run_jobnumber', 'Submitted batch job *([0-9]+)')
+            n.set('kill_command', 'scancel')
+            n.set('status_command', 'squeue -j')
+            n.set('status_running', ' (CF|CG|R|ST|S) *[0-9]')
+            n.set('status_waiting', ' (PD|SE) *[0-9]')
+        if text != 'local':
+            n.set('cache', '.cache/sjef')
+        root.write(self.file, pretty_print=True, xml_declaration=True, encoding='utf-8')
+        self.edit_combo.clear()
+        self.edit_combo.addItem('')
+        self.edit_combo.addItems(self.backends)
+        self.edit_combo.setCurrentText(name)
+        self.new_combo.setCurrentText('')
 
 
-def new(text, file):
-    print('backend.new', text, file)
-    root = etree.parse(file)
-    n = etree.SubElement(root.getroot(), 'backend')
-    n.set('name', 'new one')
-    n.set('run_command', 'molpro')
-    if text == 'remote linux':
-        n.set('host', 'someone@some.computer.somewhere')
-    if text == 'Slurm':
-        n.set('run_command', 'your_job_submission_script')
-        n.set('run_jobnumber', 'Submitted batch job *([0-9]+)')
-        n.set('kill_command', 'scancel')
-        n.set('status_command', 'squeue -j')
-        n.set('status_running', ' (CF|CG|R|ST|S) *[0-9]')
-        n.set('status_waiting', ' (PD|SE) *[0-9]')
-    if text != 'local':
-        n.set('cache', '.cache/sjef')
-    print(etree.tostring(root))
+class BackendEditor(QDialog):
+    def __init__(self, backend, parent=None):
+        super().__init__(parent)
+        self.backend = backend
+        self.parent = parent
+        self.setWindowTitle('Configure backend ' + backend)
+        layout = QVBoxLayout()
+        et= etree.parse(parent.file).xpath('//backend[@name="'+backend+'"]')[0]
+        self.fields = { field : QLineEdit(et.get(field)) for field in ['name', 'run_command', 'host', 'cache', 'status_command', 'status_running', 'status_waiting']}
+        form_layout = QFormLayout()
+        for field, editor in self.fields.items():
+            form_layout.addRow(field,editor)
+        layout.addLayout(form_layout)
+        buttons = QDialogButtonBox(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        buttons.accepted.connect(self.act)
+        buttons.rejected.connect(self.close)
+        layout.addWidget(buttons)
+
+        self.setLayout(layout)
+
+    def act(self):
+        root = etree.parse(self.parent.file)
+        backend_node = root.xpath('//backend[@name="'+self.backend+'"]')[0]
+        for field, editor in self.fields.items():
+            if editor.text():
+                backend_node.set(field,editor.text())
+            elif backend_node.get(field):
+                del backend_node.attrib[field]
+        root.write(self.parent.file, pretty_print=True, xml_declaration=True, encoding='utf-8')
+        self.close()
+
+
