9f56624dfa60b1e444dae00240e2774018b41c04
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-02-16 08:54:30 +0000

    Windows packaging

--- build.ps1
+++ build.ps1
@@ -30,7 +30,7 @@ pyinstaller --noconfirm `
 $WshShell = New-Object -comObject WScript.Shell
 $dir = Split-Path $MyInvocation.MyCommand.Path -Parent
 $Shortcut = $WshShell.CreateShortcut( ($dir + ".\dist\iMolpro\iMolpro.lnk"))
-$Shortcut.TargetPath = ".\internal_\mintty.exe"
+$Shortcut.TargetPath = ".\_internal\mintty.exe"
 $Shortcut.Arguments = "-w hide .\iMolpro.exe"
 $Shortcut.Save()
 
