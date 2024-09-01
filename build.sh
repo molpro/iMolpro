4953e7d0144dc706a894c270666af2dc7a069aa8
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-09-01 12:24:21 +0100

    Attempt to address #224
    Signed-off-by: Peter Knowles <KnowlesPJ@Cardiff.ac.uk>

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-conda install -c conda-forge -y --file=requirements.txt  || exit 1
+conda install -c conda-forge -y --file=requirements.txt python=3.11 scipy=1.11  || exit 1
 
-if [ "$(uname)" = Darwin -a $(uname -m) = x86_64 ]; then
-  conda install -c conda-forge -y scipy==1.11
-fi
+#if [ "$(uname)" = Darwin -a $(uname -m) = x86_64 ]; then
+#  conda install -c conda-forge -y scipy==1.11
+#fi
 
 if [ "$(uname)" = Darwin ]; then
   pyinstaller_opt="--windowed --osx-bundle-identifier=net.molpro.iMolpro --icon=molpro.icns"
