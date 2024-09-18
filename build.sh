e41dbc045f49f8a5d6f443a67c0747a0c07d201f
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-09-18 07:18:53 +0100

    debug 140
    Signed-off-by: Peter Knowles <KnowlesPJ@Cardiff.ac.uk>

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 conda install -c conda-forge -y --file=requirements.txt python=3.11 scipy=1.11  || exit 1
+conda list
 
 #if [ "$(uname)" = Darwin -a $(uname -m) = x86_64 ]; then
 #  conda install -c conda-forge -y scipy==1.11
