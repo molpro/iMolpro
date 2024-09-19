ba01965903f66398a30ca20cbbac26836d3da6ec
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-09-19 11:58:26 +0100

    debug 140
    Signed-off-by: Peter Knowles <KnowlesPJ@Cardiff.ac.uk>

--- database.py
+++ database.py
@@ -117,6 +117,7 @@ class DatabaseFetchDialog(QDialog):
                 import ssl
                 print('using certifi',certifi.where())
                 context = ssl.create_default_context(cafile=certifi.where())
+                print(urlopen('https://www.molpro.net',context=context))
                 print(urlopen('https://pubchem.ncbi.nlm.nih.gov',context=context))
                 print(urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/JSON?record_type=3d',urlencode([('name', query)]).encode('utf8'),context=context))
             except HTTPError as e:
