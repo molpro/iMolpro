ae0a1fba8035e47251edae0547634dba693b3954
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-11-25 09:01:33 +0000

    .

--- ProjectWindow.py
+++ ProjectWindow.py
@@ -327,7 +327,7 @@ class ProjectWindow(QMainWindow):
             button = dlg.exec()
             if button != QMessageBox.Yes:
                 logger.debug('licence not accepted; exiting')
-                QCoreApplication.quit()
+                sys.exit(1)
             else:
                 with open(licence_accepted_file, 'w') as f:
                     logger.debug('licence accepted: ' + licence_acceptance_text)
