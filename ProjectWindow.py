fe93cca9a91726c28ce08af856db170e2e733202
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2026-05-31 20:50:07 +0100

    fix

--- ProjectWindow.py
+++ ProjectWindow.py
@@ -736,10 +736,10 @@ class ProjectWindow(QMainWindow):
             for index in range(10000):
                 orbitals = self.project.orbitals(index)
                 orbitals_node = orbitals[0].node.getparent()
-                label = orbitals_node.attrib['method'] + '/' + orbitals_node.attrib['type'] + ' orbitals ' + str(index+1)
+                label = orbitals_node.attrib['method'] + '/' + orbitals_node.attrib['type'] + ' orbitals: ' + str(index+1)
                 self.vods[label] = OrbitalsWidget(orbitals, self)
         except Exception as e:
-            # print('Orbitals except',str(e))
+            print('Orbitals except',str(e))
             pass
 
 
